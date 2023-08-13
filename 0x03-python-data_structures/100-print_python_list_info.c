a63e3a2c5b089f1afabd51b3dd466d0d66c618a9
ogeorgeonyeka <ogeorgeonyeka@gmail.com>
2023-08-13 06:37:38 -0700

    task 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,12 +1,11 @@
-#inclucde "lists.h"
-#include <object.h>
+#include "lists.h"
 #include <listobject.h>
 
 /**
  * print_python_list_info - Prints basic info about Python lists.
  * @p: A PyObject list.
  */
-void print_python_list_info(PyObject * p)
+void print_python_list_info(PyObject *p)
 {
 	long int size = pyList_size(p);
 	int i;
